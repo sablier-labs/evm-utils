0d0dc8df7e2a3748ddc0c12492c1e330a941ce8a
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-05-04 16:52:06 +0300

    shub feedback

--- src/mocks/erc20/SablierERC20Faucet.sol
+++ src/mocks/erc20/SablierERC20Faucet.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.22;
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 /// @notice Allows anyone to mint or burn any amount of tokens to any account.
-contract ERC20InfiniteMint is ERC20("ERC20InfiniteMint", "EIM") {
+contract SablierERC20Faucet is ERC20("SablierERC20Faucet", "SAB-ERC20") {
     /*//////////////////////////////////////////////////////////////////////////
                                        EVENTS
     //////////////////////////////////////////////////////////////////////////*/
@@ -28,6 +28,7 @@ contract ERC20InfiniteMint is ERC20("ERC20InfiniteMint", "EIM") {
     /// @param account The address to receive the newly minted tokens.
     /// @param value The amount of tokens to mint.
     function mint(address account, uint256 value) public {
+        require(balanceOf(account) <= 10_000_000e18, "dont be greedy");
         _mint(account, value);
     }
 }
