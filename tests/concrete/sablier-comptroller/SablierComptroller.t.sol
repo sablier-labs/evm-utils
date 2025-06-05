313035b79fee18c65dce8c815af77805cdf59eab
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-06-05 16:50:55 +0300

    test: calculations

--- tests/concrete/sablier-comptroller/SablierComptroller.t.sol
+++ tests/concrete/sablier-comptroller/SablierComptroller.t.sol
@@ -9,20 +9,31 @@ import { Unit_Test } from "../../Unit.t.sol";
 abstract contract SablierComptroller_Unit_Concrete_Test is Unit_Test {
     ChainlinkOracleMock internal oracle;
     SablierComptroller internal comptroller;
+    SablierComptroller internal comptrollerZero;
 
     address internal campaignCreator;
     address internal sender;
 
     uint256 internal constant AIRDROP_MIN_FEE_USD = 3e8; // equivalent to $3
+    uint256 public constant AIRDROP_MIN_FEE_WEI = (1e18 * AIRDROP_MIN_FEE_USD) / 3000e8; // at $3000 per ETH
     uint256 internal constant AIRDROPS_CUSTOM_FEE_USD = 0.5e8; // equivalent to $0.5
+    uint256 internal constant AIRDROPS_CUSTOM_FEE_WEI = (1e18 * AIRDROPS_CUSTOM_FEE_USD) / 3000e8; // at $3000 per ETH
     uint256 internal constant FLOW_MIN_FEE_USD = 1e8; // equivalent to $1
+    uint256 internal constant FLOW_MIN_FEE_WEI = (1e18 * FLOW_MIN_FEE_USD) / 3000e8; // at $3000 per ETH
     uint256 internal constant FLOW_CUSTOM_FEE_USD = 0.1e8; // equivalent to $0.1
+    uint256 internal constant FLOW_CUSTOM_FEE_WEI = (1e18 * FLOW_CUSTOM_FEE_USD) / 3000e8; // at $3000 per ETH
     uint256 internal constant LOCKUP_MIN_FEE_USD = 1e8; // equivalent to $1
+    uint256 internal constant LOCKUP_MIN_FEE_WEI = (1e18 * LOCKUP_MIN_FEE_USD) / 3000e8; // at $3000 per ETH
     uint256 internal constant LOCKUP_CUSTOM_FEE_USD = 0.1e8; // equivalent to $0.1
+    uint256 internal constant LOCKUP_CUSTOM_FEE_WEI = (1e18 * LOCKUP_CUSTOM_FEE_USD) / 3000e8; // at $3000 per ETH
     uint256 internal constant MAX_FEE_USD = 100e8; // equivalent to $100
+    uint40 public constant FEB_1_2025 = 1_738_368_000;
 
     function setUp() public virtual override {
         Unit_Test.setUp();
+
+        // Warp to Feb 1, 2025 at 00:00 UTC to provide a more realistic testing environment.
+        vm.warp({ newTimestamp: FEB_1_2025 });
 
         campaignCreator = createUser("campaignCreator", noSpenders);
         sender = createUser("sender", noSpenders);
@@ -33,6 +44,7 @@ abstract contract SablierComptroller_Unit_Concrete_Test is Unit_Test {
         // Deploy the Sablier Comptroller with minimum fees and the mock oracle.
         comptroller =
             new SablierComptroller(admin, AIRDROP_MIN_FEE_USD, FLOW_MIN_FEE_USD, LOCKUP_MIN_FEE_USD, address(oracle));
+        comptrollerZero = new SablierComptroller(admin, 0, 0, 0, address(0));
 
         // Set the accountant role.
         setMsgSender(admin);
