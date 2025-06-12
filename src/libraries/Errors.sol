f66f310c917f845cdadd50327f9727165508d7c6
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-06-12 19:36:33 +0300

    chore: error order

--- src/libraries/Errors.sol
+++ src/libraries/Errors.sol
@@ -18,11 +18,11 @@ library Errors {
     /// @notice Thrown when `msg.sender` is not the comptroller.
     error ComptrollerManager_CallerNotComptroller(address comptroller, address caller);
 
-    /// @notice Thrown when trying to set zero as the comptroller address.
-    error ComptrollerManager_ZeroAddress();
-
     /// @notice Thrown when the fee transfer fails.
     error ComptrollerManager_FeeTransferFailed(address comptroller, uint256 feeAmount);
+
+    /// @notice Thrown when trying to set zero as the comptroller address.
+    error ComptrollerManager_ZeroAddress();
 
     /*//////////////////////////////////////////////////////////////////////////
                                     COMPTROLLER
