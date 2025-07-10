75903f12f464436459e8cfe10aed72de6bc1e1f5
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-07-10 17:58:25 +0300

    chore: emit msg.sender for FEE_MANAGEMENT_ROLE controlled functions

--- src/SablierComptroller.sol
+++ src/SablierComptroller.sol
@@ -115,7 +115,7 @@ contract SablierComptroller is ISablierComptroller, RoleAdminable {
         delete _protocolFees[protocol].customFeesUSD[user];
 
         // Log the update.
-        emit ISablierComptroller.DisableCustomFeeUSD(protocol, user);
+        emit ISablierComptroller.DisableCustomFeeUSD(protocol, msg.sender, user);
     }
 
     /// @inheritdoc ISablierComptroller
@@ -168,7 +168,7 @@ contract SablierComptroller is ISablierComptroller, RoleAdminable {
         _protocolFees[protocol].customFeesUSD[user].fee = customFeeUSD;
 
         // Log the update.
-        emit ISablierComptroller.SetCustomFeeUSD(protocol, user, customFeeUSD);
+        emit ISablierComptroller.SetCustomFeeUSD(protocol, msg.sender, user, customFeeUSD);
     }
 
     /// @inheritdoc ISablierComptroller
@@ -188,7 +188,7 @@ contract SablierComptroller is ISablierComptroller, RoleAdminable {
         _protocolFees[protocol].minFeeUSD = newMinFeeUSD;
 
         // Log the update.
-        emit ISablierComptroller.SetMinFeeUSD(protocol, previousMinFeeUSD, newMinFeeUSD);
+        emit ISablierComptroller.SetMinFeeUSD(protocol, msg.sender, previousMinFeeUSD, newMinFeeUSD);
     }
 
     /// @inheritdoc ISablierComptroller
