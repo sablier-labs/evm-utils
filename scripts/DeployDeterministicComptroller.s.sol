ae1a0181237e8c2dc7368488443d980857a069ef
smol-ninja <shubhamy2015@gmail.com>
2025-07-16 21:25:42 +0100

    script: rename variables in deployment script
    Co-authored-by: Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>

--- scripts/DeployDeterministicComptroller.s.sol
+++ scripts/DeployDeterministicComptroller.s.sol
@@ -6,18 +6,18 @@ import { SablierComptroller } from "../src/SablierComptroller.sol";
 import { BaseScript } from "../src/tests/BaseScript.sol";
 
 contract DeployDeterministicComptroller is BaseScript {
-    function run() public broadcast returns (SablierComptroller comptrollerImpl, address comptrollerProxy) {
+    function run() public broadcast returns (SablierComptroller impl, address proxyWithImpl) {
         // Deploy the comptroller implementation.
-        comptrollerImpl = new SablierComptroller{ salt: SALT }(
+        impl = new SablierComptroller{ salt: SALT }(
             getAdmin(), getInitialMinFeeUSD(), getInitialMinFeeUSD(), getInitialMinFeeUSD(), getChainlinkOracle()
         );
 
         // Deploy the comptroller proxy and initialize the state variables.
         // Note: This should be done only once when a new proxy is deployed. In the future, this must be changed to
         // handle the {upgradeToAndCall} function to update the new comptroller implementation.
-        comptrollerProxy = address(
+        proxyWithImpl = address(
             new ERC1967Proxy{ salt: SALT }({
-                implementation: address(comptrollerImpl),
+                implementation: address(impl),
                 _data: abi.encodeCall(
                     SablierComptroller.initialize,
                     (getAdmin(), getInitialMinFeeUSD(), getInitialMinFeeUSD(), getInitialMinFeeUSD(), getChainlinkOracle())
