870a3231208f663a2baf31cfa18d4b14a835a087
smol-ninja <shubhamy2015@gmail.com>
2025-07-17 12:36:51 +0100

    test: add invariant tests for comptroller

---
+++ tests/invariant/handlers/ComptrollerHandler.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.26;
+
+import { UUPSUpgradeable } from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
+import { Adminable } from "src/Adminable.sol";
+import { ISablierComptroller } from "src/interfaces/ISablierComptroller.sol";
+import { SablierComptroller } from "src/SablierComptroller.sol";
+
+contract ComptrollerHandler {
+    /*//////////////////////////////////////////////////////////////////////////
+                                  STATE VARIABLES
+    //////////////////////////////////////////////////////////////////////////*/
+
+    address public comptroller;
+
+    mapping(string functionName => uint256 count) public calls;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                    CONSTRUCTOR
+    //////////////////////////////////////////////////////////////////////////*/
+
+    constructor(address comptroller_) {
+        comptroller = comptroller_;
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                      HANDLERS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function initialize(address newAdmin) external {
+        (bool success,) =
+            comptroller.call(abi.encodeCall(ISablierComptroller.initialize, (newAdmin, 0, 0, 0, address(0))));
+
+        if (success) calls["initialize"]++;
+    }
+
+    function transferAdmin(address newAdmin) external {
+        (bool success,) = comptroller.call(abi.encodeCall(Adminable.transferAdmin, (newAdmin)));
+
+        if (success) calls["transferAdmin"]++;
+    }
+
+    function upgradeToAndCall(address newAdmin) external {
+        // Deploy a new comptroller.
+        address newImplementation = address(new SablierComptroller(newAdmin, 0, 0, 0, address(0)));
+
+        // Upgrade to the new comptroller.
+        (bool success,) = comptroller.call(abi.encodeCall(UUPSUpgradeable.upgradeToAndCall, (newImplementation, "")));
+
+        if (success) calls["upgradeToAndCall"]++;
+    }
+}
