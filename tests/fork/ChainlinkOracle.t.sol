a9472b7dada095e9aea3b42414eb461a2c516ed1
smol-ninja <shubhamy2015@gmail.com>
2025-10-12 18:48:52 +0200

    refactor: ChainId library test: refactor chainlink oracle fork test

--- tests/fork/ChainlinkOracle.t.sol
+++ tests/fork/ChainlinkOracle.t.sol
@@ -2,16 +2,28 @@
 pragma solidity >=0.8.22 <0.9.0;
 
 import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
 import { BaseScript } from "src/tests/BaseScript.sol";
-
+import { ChainId } from "src/tests/ChainId.sol";
 import { Base_Test } from "../Base.t.sol";
 
 contract BaseScriptMock is BaseScript { }
 
 contract ChainlinkOracle_Fork_Test is Base_Test {
-    /// @notice A modifier that runs the forked test for a given chain
-    modifier initForkTest(string memory chainName) {
+    function testFork_ChainlinkOracle(uint256 chainIdIndex) external {
+        // Get list of mainnet chain IDs.
+        uint256[] memory supportedChainIds = ChainId.getAllMainnets();
+
+        // Bound the chain ID index.
+        chainIdIndex = bound(chainIdIndex, 0, supportedChainIds.length - 1);
+
+        uint256 chainId = supportedChainIds[chainIdIndex];
+
+        // Since fork test on zksync requires a different foundry version, we skip it in this test.
+        vm.assume(chainId != ChainId.ZKSYNC);
+
+        // Get the chain name.
+        string memory chainName = ChainId.getName(chainId);
+
         // Fork chain on the latest block number.
         vm.createSelectFork({ urlOrAlias: chainName });
 
@@ -19,6 +31,9 @@ contract ChainlinkOracle_Fork_Test is Base_Test {
 
         // Get the Chainlink oracle address for the current chain.
         address oracle = baseScriptMock.getChainlinkOracle();
+
+        // Skip if oracle is not found.
+        vm.assume(oracle != address(0));
 
         // Retrieve the latest price and decimals from the Chainlink oracle.
         (, int256 price,, uint256 updatedAt,) = AggregatorV3Interface(oracle).latestRoundData();
@@ -30,27 +45,5 @@ contract ChainlinkOracle_Fork_Test is Base_Test {
 
         // Assert that the oracle returns 8 decimals.
         vm.assertEq(oracleDecimals, 8, "oracle decimals");
-
-        _;
     }
-
-    function testFork_ChainlinkOracle_Arbitrum() external initForkTest("arbitrum") { }
-
-    function testFork_ChainlinkOracle_Avalanche() external initForkTest("avalanche") { }
-
-    function testFork_ChainlinkOracle_Base() external initForkTest("base") { }
-
-    function testFork_ChainlinkOracle_BSC() external initForkTest("bsc") { }
-
-    function testFork_ChainlinkOracle_Ethereum() external initForkTest("ethereum") { }
-
-    function testFork_ChainlinkOracle_Gnosis() external initForkTest("gnosis") { }
-
-    function testFork_ChainlinkOracle_Linea() external initForkTest("linea") { }
-
-    function testFork_ChainlinkOracle_Optimism() external initForkTest("optimism") { }
-
-    function testFork_ChainlinkOracle_Polygon() external initForkTest("polygon") { }
-
-    function testFork_ChainlinkOracle_Scroll() external initForkTest("scroll") { }
 }
