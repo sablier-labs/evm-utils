2f1f4a17a6149fb9f25538751be168de706ad1a0
smol-ninja <shubhamy2015@gmail.com>
2025-10-16 10:15:29 +0100

    remove fuzzed paramter from chainlink oracle test

--- tests/fork/ChainlinkOracle.t.sol
+++ tests/fork/ChainlinkOracle.t.sol
@@ -9,41 +9,41 @@ import { Base_Test } from "../Base.t.sol";
 contract BaseScriptMock is BaseScript { }
 
 contract ChainlinkOracle_Fork_Test is Base_Test {
-    function testFork_ChainlinkOracle(uint256 chainIdIndex) external {
+    function testFork_ChainlinkOracle() external {
         // Get list of mainnet chain IDs.
         uint256[] memory supportedChainIds = ChainId.getAllMainnets();
 
-        // Bound the chain ID index.
-        chainIdIndex = bound(chainIdIndex, 0, supportedChainIds.length - 1);
+        // Run tests over all supported chains.
+        for (uint256 i = 0; i < 26; ++i) {
+            uint256 chainId = supportedChainIds[i];
 
-        uint256 chainId = supportedChainIds[chainIdIndex];
+            // Skip zksync as it requires a different foundry version.
+            if (chainId == ChainId.ZKSYNC) continue;
 
-        // Since fork test on zksync requires a different foundry version, we skip it in this test.
-        vm.assume(chainId != ChainId.ZKSYNC);
+            // Get the chain name.
+            string memory chainName = ChainId.getName(chainId);
 
-        // Get the chain name.
-        string memory chainName = ChainId.getName(chainId);
+            // Fork chain on the latest block number.
+            vm.createSelectFork({ urlOrAlias: chainName });
 
-        // Fork chain on the latest block number.
-        vm.createSelectFork({ urlOrAlias: chainName });
+            BaseScriptMock baseScriptMock = new BaseScriptMock();
 
-        BaseScriptMock baseScriptMock = new BaseScriptMock();
+            // Get the Chainlink oracle address for the current chain.
+            address oracle = baseScriptMock.getChainlinkOracle();
 
-        // Get the Chainlink oracle address for the current chain.
-        address oracle = baseScriptMock.getChainlinkOracle();
+            // Skip if oracle is not found.
+            if (oracle == address(0)) continue;
 
-        // Skip if oracle is not found.
-        vm.assume(oracle != address(0));
+            // Retrieve the latest price and decimals from the Chainlink oracle.
+            (, int256 price,, uint256 updatedAt,) = AggregatorV3Interface(oracle).latestRoundData();
+            uint8 oracleDecimals = AggregatorV3Interface(oracle).decimals();
 
-        // Retrieve the latest price and decimals from the Chainlink oracle.
-        (, int256 price,, uint256 updatedAt,) = AggregatorV3Interface(oracle).latestRoundData();
-        uint8 oracleDecimals = AggregatorV3Interface(oracle).decimals();
+            // Assert that the Chainlink price feed returns non-zero values.
+            vm.assertGt(uint256(price), 0, "price");
+            vm.assertGt(updatedAt, 0, "updated at");
 
-        // Assert that the Chainlink price feed returns non-zero values.
-        vm.assertGt(uint256(price), 0, "price");
-        vm.assertGt(updatedAt, 0, "updated at");
-
-        // Assert that the oracle returns 8 decimals.
-        vm.assertEq(oracleDecimals, 8, "oracle decimals");
+            // Assert that the oracle returns 8 decimals.
+            vm.assertEq(oracleDecimals, 8, "oracle decimals");
+        }
     }
 }
